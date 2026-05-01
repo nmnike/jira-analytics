0acf985a6b5960a2a45ba55842b3a2ce075619a6
akim4ik <kopyshok@users.noreply.github.com>
2026-05-01 19:16:31 +0300

    test(work-types): is_system column default + assignment

--- tests/test_mandatory_work_type.py
+++ tests/test_mandatory_work_type.py
@@ -7,3 +7,17 @@ def test_subtracts_from_pool_default(db_session):
     db_session.add(wt)
     db_session.commit()
     assert wt.subtracts_from_pool is True
+
+
+def test_is_system_default_false(db_session):
+    wt = MandatoryWorkType(code="x_default", label="X")
+    db_session.add(wt)
+    db_session.commit()
+    assert wt.is_system is False
+
+
+def test_is_system_can_be_true(db_session):
+    wt = MandatoryWorkType(code="x_system", label="X", is_system=True)
+    db_session.add(wt)
+    db_session.commit()
+    assert wt.is_system is True
